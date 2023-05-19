23c622a8c6d80857adef68ac06cbfe10a11e7be4
alanis <126874482+laniss23@users.noreply.github.com>
2023-05-19 15:58:49 -0300

    oi

---
+++ script.js
@@ -0,0 +1,8 @@
+//  var valor = confirm("vamo?")
+// if (valor == true) {
+//     alert("bora");
+// } else {
+//     alert("nao, F5 para recarregar");
+// }
+var email = prompt("digite");
+alert("o email " + email + " foi");
